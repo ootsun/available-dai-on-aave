46435f7147cdf878fd9ef855f2389a8ac2ae3832
ootsun <gabriel.delhaye@hotmail.com>
2022-05-04 14:44:31 +0200

    Fix : display the total DAI reserve with 18 decimals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import {ethers} from 'ethers';
 import {contractIsAProxy, getAbi, getImplementationAddress} from './ethereum/contractUtils.js';
 import {provider} from './ethereum/ethereumUtils.js';
-import formatThousands from 'format-thousands';
+import bigDecimal from 'js-big-decimal';
 
 const v1LPContractAddress = process.env.AAVE_V1_LENDING_POOL_CONTRACT_ADDRESS;
 const v2PDPContractAddress = process.env.AAVE_V2_PROTOCOL_DATA_PROVIDER_CONTRACT_ADDRESS;
@@ -19,14 +19,14 @@ do {
   v1DaiReserve = await getDaiReserve(v1LPContractAddress, false);
   v2DaiReserve = await getDaiReserve(v2PDPContractAddress, true);
   v2AAMDaiReserve = await getDaiReserve(v2AAMPDPContractAddress, true);
-  totalReserve = v1DaiReserve * 1 + v2DaiReserve * 1 + v2AAMDaiReserve * 1;
+  totalReserve = v1DaiReserve.add(v2DaiReserve).add(v2AAMDaiReserve);
 } while (currentBlock !== await provider.getBlockNumber());
 
 console.log(`At block ${currentBlock}`);
-console.log(`Available DAI reserve on Aave V1 : ${formatThousands(v1DaiReserve)}`);
-console.log(`Available DAI reserve on Aave V2 : ${formatThousands(v2DaiReserve)}`);
-console.log(`Available DAI reserve on Aave V2 (AAM) : ${formatThousands(v2AAMDaiReserve)}`);
-console.log(`Total available DAI reserve : ${formatThousands(totalReserve)}`);
+console.log(`Available DAI reserve on Aave V1: ${v1DaiReserve.getPrettyValue()}`);
+console.log(`Available DAI reserve on Aave V2: ${v2DaiReserve.getPrettyValue()}`);
+console.log(`Available DAI reserve on Aave V2 (AAM): ${v2AAMDaiReserve.getPrettyValue()}`);
+console.log(`Total available DAI reserve: ${totalReserve.getPrettyValue()}`);
 
 async function getDaiReserve(contractAddress, isV2) {
   try {
@@ -46,7 +46,7 @@ async function getDaiReserve(contractAddress, isV2) {
       availableLiquidity = reserveData[0];
     }
 
-    return ethers.utils.formatEther(availableLiquidity);
+    return new bigDecimal(ethers.utils.formatEther(availableLiquidity));
   } catch (error) {
     console.error(`An error occurred while retrieving DAI reserve for ${contractAddress} : `);
     console.error(error);
